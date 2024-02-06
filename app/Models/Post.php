3cf1db8a137da3fb412e2ec04c7b704e0359d570
roni-ui <ronic1996@gmail.com>
2024-02-06 11:38:26 +0700

    add Eloquent Accessor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+//use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -13,4 +15,11 @@ class Post extends Model
         'title',
         'content',
     ];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/posts/' . $image),
+        );
+    }
 }
